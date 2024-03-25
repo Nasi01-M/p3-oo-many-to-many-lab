89d7753458d3900d2f6342b5cfc47c35d3f9d5ca
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2024-03-25 13:41:38 +0300

    passed 7 tests working on the 1 failed test

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,45 @@
-class Author:
-    pass
-
-
 class Book:
-    pass
+    all_books = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all_books.append(self)
+
+class Author:
+    all_authors = []
+
+    def __init__(self, name):
+        self.name = name
+        self.contracts_list = []  # Keep track of contracts related to the author
+        Author.all_authors.append(self)
+
+    def contracts(self):
+        return self.contracts_list
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        self.contracts_list.append(contract)
+        return contract
 
 
 class Contract:
-    pass
+    all_contracts = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of the Author class")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of the Book class")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all_contracts.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all_contracts if contract.date == date]
